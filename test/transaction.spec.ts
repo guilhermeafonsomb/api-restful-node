b45d78821ae461dc5f87f04763e135c030048cbd
Guilherme Afonso <guilhermeafonsomb@gmail.com>
2023-03-30 10:30:02 -0300

    test: testing list

---
+++ test/transaction.spec.ts
@@ -0,0 +1,50 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request  from 'supertest'
+
+import { app } from '../src/app'
+
+describe('Transactions routes', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+    
+    afterAll(async () => {
+        await app.close()
+    })
+    
+    it('should be able to create a new transaction', async () => {
+       await request(app.server)
+        .post('/transactions')
+        .send({
+            title: 'new transaction',
+            amount: 5000,
+            type: 'credit'
+        })
+        .expect(201);
+    })
+
+    it('should be able to list all transaction', async () => {
+        const createTransactionResponse = await request(app.server)
+        .post('/transactions')
+        .send({
+            title: 'new transaction',
+            amount: 5000,
+            type: 'credit'
+        })
+        
+        const cookies = createTransactionResponse.get('Set-Cookie')
+
+        const listTransactionsReponse = await request(app.server)
+            .get('/transactions')
+            .set('Cookie', cookies)
+        
+        expect(listTransactionsReponse.body.transactions).toEqual([
+            expect.objectContaining({
+                title: 'new transaction',
+                amount: 5000,
+            })
+        ])
+        
+     })
+})
+
